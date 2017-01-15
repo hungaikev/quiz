826027611b8c83a60a96b0445a6c0a3928e1acab
Hungai Amuhinda <hungaiinc@gmail.com>
2017-01-15 12:34:06 +0300

    Comments

--- app/dao/AnswerDao.scala
+++ app/dao/AnswerDao.scala
@@ -51,31 +51,83 @@ class AnswersDao @Inject() (protected val dbConfigProvider: DatabaseConfigProvid
     */
   def addAnswer(answer: Answer): Future[Unit] = db.run(insertAnswer += answer).map(_ => ())
 
+  /**
+    * Saving a sequence of answers
+    * @param answers
+    * @return
+    */
   def addAnswers(answers: Seq[Answer]):Future[Unit] = db.run(this.answers ++= answers).map(_ =>())
 
+  /**
+    * Update an already existing answer
+    * @param id
+    * @param description
+    * @return
+    */
   def updateAnswer(id:Long,description:String): Future[Unit] =
     db.run(answers.filter(_.answerId === id).map(a => (a.description) ).update(description)).map(_ => ())
 
+  /**
+    * Get the answers for a particular Questions
+    * @param questionId
+    * @return
+    */
   def getAnswers(questionId:Long):Future[Seq[Answer]] = db.run(answers.filter(_.questionId === questionId).result)
 
+  /**
+    * Get answers that have been accepted for a particular answer
+    * @param questionId
+    * @return
+    */
   def getAcceptedAnswers(questionId:Long): Future[Seq[Answer]] =
     db.run(answers.filter(_.questionId === questionId).filter(_.isAccepted).result)
 
+  /**
+    * Get the answer Count for a specific question
+    * @param questionId
+    * @return
+    */
   def getAnswersCount(questionId:Long): Future[Int] = db.run(answers.filter(_.questionId === questionId).length.result)
 
+  /**
+    * Find a specific answer
+    * @param answerId
+    * @return
+    */
   def findAnswerById(answerId:Long):Future[Seq[Answer]] = db.run(answers.filter( _.answerId === answerId).result)
 
+  /**
+    * Update an answers Status on the Answers table to True
+    * @param answerId
+    * @return
+    */
   def acceptAnswersTable(answerId:Long): Future[Unit] =
     db.run(answers.filter(_.answerId === answerId).map(_.isAccepted).update(true)).map(_ => ())
 
+  /**
+    * Update a questions Status (Has Acccepted Answer) on the Questions Table to True
+    * @param questionId
+    * @return
+    */
   def acceptQuestionAnswer(questionId:Long):Future[Unit] =
     db.run(questions.filter(_.questionId === questionId).map(_.hasAcceptedAnswer).update(true)).map(_ => ())
 
+  /**
+    * Accept an answer
+    * @param questionId
+    * @param answerId
+    * @return
+    */
   def acceptAnswer(questionId:Long,answerId:Long): Future[Unit] = for {
     _   <- acceptAnswersTable(answerId)
     _   <- acceptQuestionAnswer(questionId)
   } yield ()
 
+  /**
+    * Delete an answer
+    * @param id
+    * @return
+    */
   def deleteAnswer(id:Long): Future[Unit] = db.run(answers.filter(_.answerId === id).delete).map(_ => ())
 
 
--- app/dao/QuestionDao.scala
+++ app/dao/QuestionDao.scala
@@ -40,20 +40,58 @@ class QuestionsDao @Inject() (protected val dbConfigProvider: DatabaseConfigProv
 
   lazy val insertQuestion = questions returning questions.map(_.questionId)
 
+  /**
+    * Save a question to the database
+    * @param question
+    * @return
+    */
   def addQuestion(question: Question) : Future[Long] = db.run(insertQuestion += question)
 
+  /**
+    * Save a sequence or list of questions to the database
+    * @param questions
+    * @return
+    */
   def addQuestions(questions: Seq[Question]): Future[Unit] = db.run(this.questions ++= questions).map(_ => ())
 
+  /**
+    * Get all Unanswered Questions
+    * Filter -> Has Accepted Answer  (False)
+    * @return
+    */
   def getUnanswered:Future[Seq[Question]] = db.run(questions.filter(_.hasAcceptedAnswer === false).result)
 
+  /**
+    * Get all Answered Questions
+    * Filter -> Has Accepted Answer (True)
+    * @return
+    */
   def getAnswered : Future[Seq[Question]] = db.run(questions.filter(_.hasAcceptedAnswer === true).result)
 
+  /**
+    * Get a Question using a question Id
+    * @param questionId
+    * @return
+    */
   def findQuestionById(questionId:Long): Future[Question] = db.run(questions.filter(_.questionId === questionId).result.head)
 
+  /**
+    * Get all questions stored in the database
+    * @return
+    */
   def allQuestions:Future[Seq[Question]] = db.run(questions.result)
 
+  /**
+    * Get the Number of Questions in the Database
+    * @return
+    */
   def getQuestionCount(): Future[Int] = db.run(questions.length.result)
 
+  /**
+    * Delete a Question from the Database
+    * @param id
+    * @return
+    */
   def deleteQuestion(id:Long): Future[Unit] = db.run(questions.filter(_.questionId === id).delete).map(_ => ())
 
 
